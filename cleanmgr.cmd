8c1daa199988a97975367bd2450e6e8b4a8ee3f6
Anthony Fontanez <ajfrcc@gmail.com>
2017-12-28 15:02:27 -0500

    Delete cleanmgr.cmd

--- cleanmgr.cmd
+++
@@ -1,7 +0,0 @@
-REM Author: Anthony Fontanez
-REM Email: ajfrcc@gmail.com
-REM Version: 1.0
-REM Description: Copies CLEANMGR files from WINSXS folder to System32 so it can be used without installing the Desktop Experience role on servers
-@echo off
-copy %WINDIR%\winsxs\amd64_microsoft-windows-cleanmgr_31bf3856ad364e35_6.1.7600.16385_none_c9392808773cd7da\cleanmgr.exe %WINDIR%\System32\
-copy %WINDIR%\winsxs\amd64_microsoft-windows-cleanmgr.resources_31bf3856ad364e35_6.1.7600.16385_en-us_b9cb6194b257cc63\cleanmgr.exe.mui %WINDIR%\System32\en-US\
